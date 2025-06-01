0e4f6e20f7f0a06ba4a347f5102f56086720cf93
shreyapancholi05 <138470284+shreyapancholi05@users.noreply.github.com>
2025-06-01 18:55:33 +0530

    Update interleave-the-first-half-of-the-queue-with-second-half.cpp

--- Difficulty: Medium/Interleave the First Half of the Queue with Second Half/interleave-the-first-half-of-the-queue-with-second-half.cpp
+++ Difficulty: Medium/Interleave the First Half of the Queue with Second Half/interleave-the-first-half-of-the-queue-with-second-half.cpp
@@ -1,3 +1,4 @@
+// using queue
 class Solution {
   public:
     queue<int> rearrangeQueue(queue<int> q) {
@@ -13,6 +14,47 @@ class Solution {
         while(!qu.empty()){
             int val = qu.front();
             qu.pop();
+            q.push(val);
+            val = q.front();
+            q.pop();
+            q.push(val);
+        }
+        return q;
+    }
+};
+
+// using stack
+class Solution {
+  public:
+    queue<int> rearrangeQueue(queue<int> q) {
+        stack<int> s;
+        
+        int n = q.size();
+        for(int i=0; i<n/2; i++){
+            s.push(q.front());
+            q.pop();
+            
+        }
+        while(!s.empty()){
+        int val = s.top();
+        s.pop();
+        q.push(val);
+        }
+        
+        for(int i=0 ; i< n/2 ; i++){
+            q.push(q.front());
+            q.pop();
+            
+        }
+        
+        for(int i=0; i<n/2; i++){
+            s.push(q.front());
+            q.pop();
+        }
+        
+        while(!s.empty()){
+            int val = s.top();
+            s.pop();
             q.push(val);
             val = q.front();
             q.pop();
