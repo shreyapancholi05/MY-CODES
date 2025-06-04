8da740ab093b2a88de8f2c63e273e74e03eb8c48
shreyapancholi05 <138470284+shreyapancholi05@users.noreply.github.com>
2025-06-04 15:31:26 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Largest Element in Array/largest-element-in-array.cpp
@@ -0,0 +1,20 @@
+class Solution {
+  public:
+    int largest(vector<int> &arr) {
+        // code here
+
+        int largest = arr[0];
+
+        for(int i=1; i<arr.size(); i++){
+            if(arr[i] > largest){
+                largest = arr[i];
+            }
+            
+        }
+        return largest;
+    }
+
+
+        
+    
+};
