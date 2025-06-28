3b4f7f7e47adee4a8218883dccc7856a122ce15d
shreyapancholi05 <138470284+shreyapancholi05@users.noreply.github.com>
2025-06-28 09:59:26 +0530

    Time: 0 ms (100%), Space: 33.3 MB (61.27%) - LeetHub

--- 1020-number-of-enclaves/1020-number-of-enclaves.cpp
+++ 1020-number-of-enclaves/1020-number-of-enclaves.cpp
@@ -1,36 +1,31 @@
+using vi = vector<int>;
+using vvi = vector<vi>;
+
 class Solution {
-    vector<int> dx = {-1, 1, 0 , 0};
+    vector<int> dx = {-1, 1, 0, 0};
     vector<int> dy = {0, 0, -1, 1};
-    void dfs(int i,int j, vector<vector<int>> &grid,int m, int n){
-        if(i<0 || j<0 || i>=m || j>=n ||grid[i][j] == 0) 
+    int m, n;
+    void dfs(int i, int j, vvi& grid) {
+        if (i < 0 || j < 0 || i >= m || j >= n || grid[i][j] == 0)
             return;
-        grid[i][j] = 0;
-        /*
-        dfs(i-1, j, grid, m, n);
-        dfs(i+1 , j, grid, m, n);
-        dfs(i ,j-1 ,grid, m, n);
-        dfs(i, j+1 , grid, m ,n);
-            */
-        for(int dir=0; dir<4; dir++){
-            dfs(i + dx[dir] , j + dy[dir] , grid, m, n);
-        }
+        grid[i][j] = 0; // water cell ban gaya
+        for (int dir = 0; dir < 4; dir++)
+            dfs(i + dx[dir], j + dy[dir], grid);
     }
+
 public:
     int numEnclaves(vector<vector<int>>& grid) {
-        int m = grid.size();
-        int n = grid[0].size();
-        for(int i=0; i<m; i++)
-            for(int j=0; j<n; j++)
-                if(i == 0 || j==0 || i == m-1 || j == n-1)
-                    if(grid[i][j] == 1)
-                        dfs(i ,j ,grid,m,n);
-            
+        m = grid.size(), n = grid[0].size();
+        for (int i = 0; i < m; i++)
+            for (int j = 0; j < n; j++)
+                if (i == 0 || j == 0 || i == m - 1 || j == n - 1) // boundary
+                    if (grid[i][j] == 1)
+                        dfs(i, j, grid);
         int cnt = 0;
-        for(int i=0; i<m; i++)
-            for(int j=0; j<n; j++)
-                if(grid[i][j] == 1)
+        for (int i = 0; i < m; i++)
+            for (int j = 0; j < n; j++)
+                if (grid[i][j] == 1)
                     cnt++;
         return cnt;
-
     }
 };
