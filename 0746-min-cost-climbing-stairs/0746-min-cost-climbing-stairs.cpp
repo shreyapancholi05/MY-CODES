fcc79ff9362269f628cd19c0b8ede00d578d3b7c
shreyapancholi05 <138470284+shreyapancholi05@users.noreply.github.com>
2025-07-01 17:43:59 +0530

    Time: 0 ms (100%), Space: 17.3 MB (96.95%) - LeetHub

--- 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.cpp
+++ 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.cpp
@@ -1,5 +1,6 @@
 class Solution {
-    int solve(vector<int> &cost, int n, vector<int> &dp){
+    /*recursion+memoization*/
+    int solve1(vector<int> &cost, int n, vector<int> &dp){
         if(n == 0)
             return cost[0];
 
@@ -8,14 +9,45 @@ class Solution {
 
         if(dp[n] != -1)
             return dp[n];
-        dp[n] = cost[n] + min(solve(cost, n-1, dp), solve(cost, n-2, dp));
+        dp[n] = cost[n] + min(solve1(cost, n-1, dp), solve1(cost, n-2, dp));
         return dp[n];
+    }
+    /*tabulation*/
+    int solve2(vector<int> &cost, int n){
+        vector<int> dp(n+1, -1);
+        dp[0] = cost[0];
+        dp[1] = cost[1];
+
+        for(int i=2; i<n ; i++){
+            dp[i] = cost[i] + min(dp[i-1] , dp[i-2]);
+
+        }
+
+        return min(dp[n-1] , dp[n-2]);
+
+    }
+    /*spce optimization*/
+    int solve3(vector<int> &cost, int n){
+        int prev2 = cost[0];
+        int prev1 = cost[1];
+
+        for(int i=2; i<n; i++){
+            int curr = cost[i] + min(prev2, prev1);
+            prev2 = prev1;
+            prev1 = curr;
+        }
+
+        return min(prev1, prev2);
     }
 public:
     int minCostClimbingStairs(vector<int>& cost) {
-        int n= cost.size();
+        /*int n= cost.size();
         vector<int> dp(n+1, -1);
         int ans = min(solve(cost, n-1, dp), solve(cost, n-2, dp));
         return ans;
+        */
+        int n = cost.size();
+        // return solve2(cost, n);
+        return solve3(cost, n);
     }
 };
