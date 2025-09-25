89721cc480d8f4254b42bd82fd6f998c5b5d0778
shreyapancholi05 <138470284+shreyapancholi05@users.noreply.github.com>
2025-09-25 17:35:22 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Nth Fibonacci Number/nth-fibonacci-number.cpp
@@ -0,0 +1,19 @@
+// User function Template for C++
+class Solution {
+  public:
+    int solve(int n, vector<int> &dp){
+        if(n <= 1) return n;
+        
+        
+        if(dp[n] != -1) return dp[n];
+        
+        dp[n] = solve(n-1, dp) + solve(n-2, dp);
+        return dp[n];
+    }
+    int nthFibonacci(int n) {
+        // code here
+        vector<int> dp(n+1, -1);
+       
+        return solve(n, dp);
+    }
+};
