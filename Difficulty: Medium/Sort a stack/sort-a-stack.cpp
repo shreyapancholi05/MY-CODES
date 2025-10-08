1b9e2dfd58c6e457e0ae1186cb82e189257a007a
shreyapancholi05 <138470284+shreyapancholi05@users.noreply.github.com>
2025-10-08 18:29:29 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Sort a stack/sort-a-stack.cpp
+++ Difficulty: Medium/Sort a stack/sort-a-stack.cpp
@@ -1,25 +1,23 @@
 class Solution {
   public:
-    void insertsorted(stack<int> &st, int element){
-        if(st.empty() || st.top() <= element){
-            st.push(element);
+    void insertsorted(stack<int> &st, int n){
+        if(st.empty() || st.top()<=n){
+            st.push(n);
             return;
         }
         
-        int newel = st.top();
+        int num = st.top();
         st.pop();
         
-        insertsorted(st, element);
         
-        st.push(newel);
-        
+        insertsorted(st, n);
+        st.push(num);
     }
     void sortStack(stack<int> &st) {
         // code here
         if(st.empty()){
             return;
         }
-        
         int num = st.top();
         st.pop();
         
