98ac19f6cecd021f64f4e4a86b497519b62e2126
shreyapancholi05 <138470284+shreyapancholi05@users.noreply.github.com>
2025-10-24 07:52:01 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Selection Sort/selection-sort.c
+++ Difficulty: Easy/Selection Sort/selection-sort.c
@@ -1,13 +1,11 @@
 void selectionSort(int arr[], int n) {
     // Code here
-    int mini;
-
+    int mini = 0;
     for(int i=0; i<=n-2; i++){
         mini = i;
-        for(int j=i; j<=n-1; j++){
+        for(int j=i; j<n; j++){
             if(arr[j] < arr[mini]){
-                mini = j;
-                
+                mini  = j;
             }
         }
         int temp = arr[mini];
